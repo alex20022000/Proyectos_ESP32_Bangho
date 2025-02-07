57b02009193e5d746b3ebd65ea9f94346e43cec5
alex20022000 <alexh.drp@gmail.com>
2025-02-06 21:07:46 -0300

    Agrego pequeño cambio en ejercicio 2.1

--- Ejercicios/2.1_Semaforos_Binarios/src/main.c
+++ Ejercicios/2.1_Semaforos_Binarios/src/main.c
@@ -22,7 +22,8 @@ void tskVirtualButton(void *pvParameters);
 void tskWaitForEvent(void *pvParameters);
 void setUPTasks();
 
-void app_main() {
+void app_main()
+{
     // Crear semáforo binario
     Semaphore = xSemaphoreCreateBinary();
 
@@ -30,27 +31,36 @@ void app_main() {
     setUPTasks();
 }
 
-void setUPTasks() {
+void setUPTasks()
+{
     xTaskCreate(tskVirtualButton, "taskButton", 2048, NULL, 1, NULL);
     xTaskCreate(tskWaitForEvent, "taskReceiver", 2048, NULL, 1, NULL);
 }
 
 // Tarea que simula un botón virtual y libera el semáforo cada 2 segundos
-void tskVirtualButton(void *pvParameters) {
-    while (1) {
+void tskVirtualButton(void *pvParameters)
+{
+    for (int i=0; i<5; i++)
+    {
         printf("Presionando botón virtual\n");
-        xSemaphoreGive(Semaphore); // Libera el semáforo
+        xSemaphoreGive(Semaphore);       // Libera el semáforo
         vTaskDelay(pdMS_TO_TICKS(2000)); // Espera 2 segundos antes de repetir
     }
+    vTaskDelete(NULL); // Destruyo la tarea para que no se reinicie el esp32
 }
 
 // Tarea que espera el evento (semáforo) y maneja timeout
-void tskWaitForEvent(void *pvParameters) {
-    while (1) {
+void tskWaitForEvent(void *pvParameters)
+{
+    while (1)
+    {
         // Espera hasta 3 segundos por el semáforo
-        if (xSemaphoreTake(Semaphore, pdMS_TO_TICKS(3000)) == pdTRUE) {
+        if (xSemaphoreTake(Semaphore, pdMS_TO_TICKS(3000)) == pdTRUE)
+        {
             printf("Evento recibido\n");
-        } else {
+        }
+        else
+        {
             printf("Timeout alcanzado\n");
         }
     }
