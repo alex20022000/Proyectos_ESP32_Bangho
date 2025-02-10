90c7bcb5d50d268d7e80807f0541f46a71268887
Alex_Bangho <adalle@hitec.com>
2025-02-10 11:19:25 -0300

    Agrego ejercicio 4.2

--- Ejercicios/4.2_Software_Timers/src/main.c
+++ Ejercicios/4.2_Software_Timers/src/main.c
@@ -14,14 +14,33 @@ Iniciar el Timer en el app_main().
 #include <freertos/timers.h>
 
 void TreeSecTimerCallback(TimerHandle_t xTimer);
+void FiveSecTimerCallback(TimerHandle_t xTimer2);
 
 void app_main()
 {
-    // Crear timer
+    // Crear timer n°1
     TimerHandle_t xTimer = xTimerCreate("TreeSecTimer", pdMS_TO_TICKS(3000), pdTRUE, 0, TreeSecTimerCallback);
+    // Crear timer n°2
+    TimerHandle_t xTimer2 = xTimerCreate("FiveSecTimer", pdMS_TO_TICKS(6000), pdTRUE, 0, FiveSecTimerCallback);
+
+    if (xTimer != NULL && xTimer2 != NULL)
+    {
+        // Iniciamos timer
+        xTimerStart(xTimer, 0);
+        xTimerStart(xTimer2, 0);
+    }
+    else
+    {
+        printf("Error al crear los timers\n");
+    }
 }
 
 void TreeSecTimerCallback(TimerHandle_t xTimer)
 {
-    printf("Timer activado\n");
+    printf("3 sec Timer1 activado\n");
+}
+
+void FiveSecTimerCallback(TimerHandle_t xTimer2)
+{
+    printf("6 sec Timer2 activado\n");
 }
